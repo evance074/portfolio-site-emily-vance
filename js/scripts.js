1f7d74d41340887433d675cfd56c6561f3810705
evance074 <emilyvance074@gmail.com>
2025-04-13 22:22:00 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
 console.log("hello world");
+
+function menuToggle() {
+    var x = document.getElementById('mynavtoggle');
+    if (x.className === 'navtoggle') {
+        x.className += ' responsive';
+      } else {
+        x.className = 'navtoggle';4
+      }
+}
