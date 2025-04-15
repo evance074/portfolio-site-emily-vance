f7d9ac4848e12764d3a6114a35211cabcdb41c71
evance074 <emilyvance074@gmail.com>
2025-04-15 11:34:26 -0400

    Update scripts.js
    add smooth scrolling

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,19 @@
-console.log("hello world");
-
 function menuToggle() {
     var x = document.getElementById('mynavtoggle');
     if (x.className === 'navtoggle') {
         x.className += ' responsive';
       } else {
-        x.className = 'navtoggle';4
+        x.className = 'navtoggle';
       }
 }
+
+$('a[href^="#"]').on('click', function (event) {
+  var target = $($(this).attr('href'));
+  if (target.length) {
+    event.preventDefault();
+    $('html, body').animate({
+      scrollTop: target.offset().top
+    }, 600);
+    menuToggle(); // Close the menu on click
+  }
+});
